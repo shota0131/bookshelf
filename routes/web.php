01329f830f67cb61732e5bb60796114c5f3e3752
shota0131 <shota.kusu131@gmail.com>
2026-09-26 03:30:03 +0900

    認証不要のルートの修正

--- routes/web.php
+++ routes/web.php
@@ -126,43 +126,45 @@ Route::middleware('auth')->group(function () {
     // 読書計画削除
     Route::delete('/reading-plans/{plan}', [ReadingPlanController::class, 'destroy'])
         ->name('reading-plans.destroy');
+
+    // ==============================
+    // ジャンル関連（認証必須）
+    // ==============================
+
+    // ジャンル一覧
+    Route::get('/genres', [GenreController::class, 'index'])
+        ->name('genres.index');
+
+    // ジャンル登録画面
+    Route::get('/genres/create', [GenreController::class, 'create'])
+        ->name('genres.create');
+
+    // ジャンル登録処理
+    Route::post('/genres', [GenreController::class, 'store'])
+        ->name('genres.store');
+
+    // ジャンル詳細
+    Route::get('/genres/{genre}', [GenreController::class, 'show'])
+        ->name('genres.show');
+
+    // ジャンル編集画面
+    Route::get('/genres/{genre}/edit', [GenreController::class, 'edit'])
+        ->name('genres.edit');
+
+    // ジャンル更新処理
+    Route::put('/genres/{genre}', [GenreController::class, 'update'])
+        ->name('genres.update');
+
+    // ジャンル削除
+    Route::delete('/genres/{genre}', [GenreController::class, 'destroy'])
+        ->name('genres.destroy');
 });
 
-// ==============================
-// 認証不要のルート
-// ==============================
+
 
 // 書籍詳細
 Route::get('/books/{book}', [BookController::class, 'show'])
     ->name('books.show');
-
-// ジャンル一覧
-Route::get('/genres', [GenreController::class, 'index'])
-    ->name('genres.index');
-
-// ジャンル登録画面
-Route::get('/genres/create', [GenreController::class, 'create'])
-    ->name('genres.create');
-
-// ジャンル登録処理
-Route::post('/genres', [GenreController::class, 'store'])
-    ->name('genres.store');
-
-// ジャンル詳細
-Route::get('/genres/{genre}', [GenreController::class, 'show'])
-    ->name('genres.show');
-
-// ジャンル編集画面
-Route::get('/genres/{genre}/edit', [GenreController::class, 'edit'])
-    ->name('genres.edit');
-
-// ジャンル更新処理
-Route::put('/genres/{genre}', [GenreController::class, 'update'])
-    ->name('genres.update');
-
-// ジャンル削除
-Route::delete('/genres/{genre}', [GenreController::class, 'destroy'])
-    ->name('genres.destroy');
 
 // ランキング
 Route::get('/ranking', [RankingController::class, 'index'])
